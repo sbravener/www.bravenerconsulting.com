aed4bda6bedc73993d5e7bc3d7baeb2d00a6d146
sbravener <sbravener@gmail.com>
2025-08-21 09:10:53 -0400

    include privacy policy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,1 @@
-function toggleMenu(){const m=document.getElementById('menu');m.classList.toggle('show')} const y=document.getElementById('year');if(y) y.textContent=new Date().getFullYear();
+const y=document.getElementById('year'); if(y) y.textContent=new Date().getFullYear();
